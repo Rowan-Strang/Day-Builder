569c6de7efee0024d9565b8da3bfdad6f49643b0
Rowan Strang <rowanstrang@gmail.com>
2024-09-23 16:48:04 +1200

    autocomplete inital commit

---
+++ client/components/AddressSearch.tsx
@@ -0,0 +1,70 @@
+import { useEffect } from 'react'
+import { Label } from '@/components/ui/label'
+
+interface AddressSearchProps {
+  onAddressSelect: (address: string) => void // Define a prop to handle address selection
+}
+
+const apiKey = import.meta.env.VITE_GOOGLE_API
+
+function AddressSearch({ onAddressSelect }: AddressSearchProps) {
+  useEffect(() => {
+    const initAutocomplete = () => {
+      const input = document.getElementById('autocomplete')
+      const autocomplete = new window.google.maps.places.Autocomplete(input, {
+        types: ['geocode'],
+        componentRestrictions: { country: 'NZ' },
+      })
+
+      autocomplete.addListener('place_changed', () => {
+        const place = autocomplete.getPlace()
+        if (place.geometry && place.geometry.location) {
+          const address = place.formatted_address || ''
+          // const lat = place.geometry.location.lat()
+          // const lng = place.geometry.location.lng()
+          // console.log('Coordinates:', { lat, lng })
+          onAddressSelect(address)
+        } else {
+          console.log('No geometry information available for this place.')
+        }
+        // console.log('Selected place:', place)
+        // Do something with the selected place data
+      })
+    }
+
+    // Ensure Google Maps script is loaded and initialize the autocomplete
+    const handleScriptLoad = () => {
+      if (!window.google) {
+        console.error('Google Maps API not loaded')
+        return
+      }
+      initAutocomplete()
+    }
+
+    // Load the Google Maps script
+    const script = document.createElement('script')
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places`
+    script.async = true
+    script.onload = handleScriptLoad
+    document.body.appendChild(script)
+
+    // Cleanup the script on component unmount
+    return () => {
+      document.body.removeChild(script)
+    }
+  }, [onAddressSelect])
+
+  return (
+    <>
+      <Label htmlFor="location">Address:</Label>
+      <input
+        id="autocomplete"
+        placeholder=" "
+        className="rounded-md border-2 border-gray-300 p-2 focus:border-blue-500"
+        type="text"
+      />
+    </>
+  )
+}
+
+export default AddressSearch
